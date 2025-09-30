b225fe548e767faaf9d8d336fffd6d1222dfdacb
mouram chafikE <mrmouramito@gmail.com>
2025-09-30 21:54:00 +0200

    add icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,7 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      keyframes: {
-        slideIn: {
-          "0%": { transform: "translateX(100%)", opacity: "0" },
-          "100%": { transform: "translateX(0)", opacity: "1" },
-        },
-      },
-      animation: {
-        slideIn: "slideIn 0.6s ease-out",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
